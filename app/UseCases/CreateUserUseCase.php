258cdab0267935e60867f4cd0d3b6c09d1f15f08
Latik <latkovskyi@gmail.com>
2018-01-04 16:44:15 +0200

    add CreateUser UseCase

---
+++ app/UseCases/CreateUserUseCase.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\UseCases;
+
+use App\Commands\CreateUserCommand;
+use App\Contracts\UserRepository;
+use App\User;
+
+final class CreateUserUseCase
+{
+    private $userRepository;
+
+    public function __construct(UserRepository $repository)
+    {
+        $this->userRepository = $repository;
+    }
+
+    public function handle(CreateUserCommand $command): void
+    {
+        try {
+            if (null !== $this->userRepository->findOneBy('email', $command->getEmail())) {
+                throw new \DomainException(\sprintf('User with email: %s already exists', $command->getEmail()));
+            }
+
+            $user = new User();
+            $user->fill([
+              'name' => $command->getName(),
+              'email' => $command->getEmail(),
+              'password' => $command->getPassword(),
+            ]);
+
+            $this->userRepository->save($user);
+        } catch (\Exception $exception) {
+            throw new \DomainException(\sprintf('Unable create new user: %s ', $exception->getMessage()));
+        }
+    }
+}
